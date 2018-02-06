9891d28a4a964251b0b7868054cae87a78810cb5
Crypton07 <step.93.07@gmail.com>
2018-02-06 18:04:57 +0700

    Creating NewUserWelcomeContract and NewUserWelcomePresenter.

--- app/src/main/java/com/blockchain/store/playmarket/ui/navigation_view/NavigationViewFragment.java
+++ app/src/main/java/com/blockchain/store/playmarket/ui/navigation_view/NavigationViewFragment.java
@@ -1,20 +1,78 @@
 package com.blockchain.store.playmarket.ui.navigation_view;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.blockchain.store.playmarket.R;
+import com.blockchain.store.playmarket.utilities.ToastUtil;
+import com.blockchain.store.playmarket.utilities.data.ClipboardUtils;
+import com.blockchain.store.playmarket.utilities.net.APIUtils;
 
+import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.blockchain.store.playmarket.utilities.crypto.CryptoUtils.keyManager;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NavigationViewFragment extends Fragment {
+
+    // Объявление кнопок на панели навигации.
+    // Закрыь панель навигации.
+    @BindView(R.id.close_image_button)
+    ImageButton closeImageButton;
+
+    // Пополнить счёт.
+    @BindView(R.id.account_top_up_button)
+    Button accountTopUpButton;
+
+
+    // Объявление слоёв на панели навигации.
+    // Список желаний.
+    @BindView(R.id.wishlist_layout)
+    LinearLayout wishlistLayout;
+
+    // Библиотека.
+    @BindView(R.id.library_layout)
+    LinearLayout libraryLayout;
+
+    // Библиотека.
+    @BindView(R.id.news_layout)
+    LinearLayout newsLayout;
+
+    // История покупок.
+    @BindView(R.id.history_layout)
+    LinearLayout historyLayout;
+
+    // Настройки.
+    @BindView(R.id.settings_layout)
+    LinearLayout settingsLayout;
+
+    // Библиотека.
+    @BindView(R.id.ether_exchange_layout)
+    LinearLayout etherExchangeLayout;
+
+    // Мои ICO.
+    @BindView(R.id.my_ico_layout)
+    LinearLayout myIcoLayout;
+
+    // О приложении.
+    @BindView(R.id.about_layout)
+    LinearLayout aboutLayout;
 
 
     public NavigationViewFragment() {
@@ -27,7 +85,21 @@ public class NavigationViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation_view, container, false);
         ButterKnife.bind(this, view);
+
         return view;
     }
 
+
+    // Метод закрытия панели навигации.
+    @OnClick(R.id.close_image_button) void onCloseImageClicked(){
+        DrawerLayout drawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    // Метод открытия диалога для пополнения счёта.
+    @OnClick(R.id.account_top_up_button) void showAddFundsDialog(){
+        final Dialog d = new Dialog(getContext());
+        d.setContentView(R.layout.add_funds_dialog);
+        d.show();
+    }
 }
--- app/src/main/java/com/blockchain/store/playmarket/ui/new_user_welcome_activity/NewUserWelcomeActivity.java
+++ app/src/main/java/com/blockchain/store/playmarket/ui/new_user_welcome_activity/NewUserWelcomeActivity.java
@@ -1,14 +1,18 @@
 package com.blockchain.store.playmarket.ui.new_user_welcome_activity;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blockchain.store.playmarket.Application;
 import com.blockchain.store.playmarket.R;
@@ -28,7 +32,8 @@ import io.ethmobile.ethdroid.KeyManager;
 public class NewUserWelcomeActivity extends AppCompatActivity {
     private static final String TAG = "NewUserWelcomeActivity";
 
-    @BindView(R.id.address_text_view) TextView addressTextView;
+    @BindView(R.id.address_text_view)
+    TextView addressTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,38 @@ public class NewUserWelcomeActivity extends AppCompatActivity {
         if (getIntent() != null) {
             addressTextView.setText(getIntent().getStringExtra(Constants.WELCOME_ACTIVITY_ADDRESS_EXTRA));
         }
+    }
+
+
+    /*
+    Метод перехода на экран отправки почты "send_mail_dialog".
+    Вызывается после нажатия на кнопку "save_mail_imageButton" пользователем.
+    */
+    @OnClick(R.id.save_mail_imageButton)
+    void sendMail(){
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        //emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, "step.93.07@gmail.com");
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Your key");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, addressTextView.getText());
+
+        this.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+    }
+
+    @OnClick(R.id.local_save_imageButton)
+    void localSaveKey(){
+
+    }
+
+    /*
+    Метод перехода на главный экран приложения "MainMenuActivity".
+    Вызывается после нажатия на кнопку "continue_button" пользователем.
+    */
+    @OnClick(R.id.continue_button)
+    void goToMainActivity(){
+        Intent myIntent = new Intent(getApplicationContext(), MainMenuActivity.class);
+        startActivity(myIntent);
+        finish();
     }
 
     @Override
@@ -92,9 +129,4 @@ public class NewUserWelcomeActivity extends AppCompatActivity {
         ToastUtil.showToast(R.string.wallet_backup_copied);
     }
 
-    public void goToMainActivity(View view) {
-        Intent myIntent = new Intent(getApplicationContext(), MainMenuActivity.class);
-        startActivity(myIntent);
-        finish();
-    }
 }
