e552cc2dca31cbcc7dbbece139471884b3a0902e
Igor Sakovich <Overetch@gmail.com>
2018-07-27 20:26:33 +0700

    code clean. Change review dialog

---
+++ app/src/main/java/com/blockchain/store/playmarket/ui/unlock_account_screen/UnlockAccountActivity.java
@@ -0,0 +1,15 @@
+package com.blockchain.store.playmarket.ui.unlock_account_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blockchain.store.playmarket.R;
+
+public class UnlockAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock_account);
+    }
+}
