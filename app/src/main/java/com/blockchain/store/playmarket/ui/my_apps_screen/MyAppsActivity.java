16d5c6b8fb63a59fef5498d6768df5f52f84b588
Igor Sakovich <Overetch@gmail.com>
2018-08-09 17:05:50 +0700

    work with myApps screen.

--- app/src/main/java/com/blockchain/store/playmarket/ui/my_apps_screen/MyAppsActivity.java
+++ app/src/main/java/com/blockchain/store/playmarket/ui/my_apps_screen/MyAppsActivity.java
@@ -1,14 +1,27 @@
 package com.blockchain.store.playmarket.ui.my_apps_screen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.blockchain.store.playmarket.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MyAppsActivity extends AppCompatActivity implements MyAppsContract.View {
     private static final String TAG = "MyAppsActivity";
+
+    @BindView(R.id.top_layout_app_name) TextView toolbarTitle;
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.progress_bar) ProgressBar progressBar;
+    @BindView(R.id.error_holder) LinearLayout errorHolder;
 
     private MyAppsPresenter presenter;
 
@@ -23,5 +36,17 @@ public class MyAppsActivity extends AppCompatActivity implements MyAppsContract.
     private void attachPresenter() {
         presenter = new MyAppsPresenter();
         presenter.init(this);
+        presenter.getApps();
     }
+
+    @OnClick(R.id.error_view_repeat_btn)
+    public void error_view_repeat_btn() {
+        // TODO
+    }
+
+    @OnClick(R.id.top_layout_back_arrow)
+    public void onBackArrowClicked() {
+        this.onBackPressed();
+    }
+
 }
--- app/src/main/java/com/blockchain/store/playmarket/ui/my_apps_screen/MyAppsPresenter.java
+++ app/src/main/java/com/blockchain/store/playmarket/ui/my_apps_screen/MyAppsPresenter.java
@@ -1,10 +1,21 @@
 package com.blockchain.store.playmarket.ui.my_apps_screen;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import com.blockchain.store.playmarket.utilities.MyPackageManager;
+
+import java.util.List;
+
 public class MyAppsPresenter implements MyAppsContract.Presenter {
     private MyAppsContract.View view;
 
     @Override
     public void init(MyAppsContract.View view) {
         this.view = view;
+    }
+
+    public void getApps() {
+        List<ApplicationInfo> allInstalledApps = MyPackageManager.getAllInstalledApps();
     }
 }
