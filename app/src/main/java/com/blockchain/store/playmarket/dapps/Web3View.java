188d0df59764635816237e1cc0a036f965e2ae5b
Overetch <Overetch@gmail.com>
2019-03-13 16:28:53 +0700

    Start work with dapp browser.

---
+++ app/src/main/java/com/blockchain/store/playmarket/dapps/Web3View.java
@@ -0,0 +1,16 @@
+package com.blockchain.store.playmarket.dapps;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import wendu.dsbridge.DWebView;
+
+public class Web3View extends DWebView {
+    public Web3View(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Web3View(Context context) {
+        super(context);
+    }
+}
